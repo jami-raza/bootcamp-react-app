aac6053917b3196c1d2fe96b2826753d09769977
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-06-04 14:50:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Bootcamp 2020 React app</h1>
         <p>
-          Hello Devrlopers.
+          Hello Devrlopers
         </p>
         <h1>Bootcamp 2020 React app</h1>
         <a
