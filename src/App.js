bbe9b796e7606d5550bc6cfd6781f5a7884adc65
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-06-10 16:09:51 +0500

    update dinner.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import Dinner from './ Dinner.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div classname="App">
+    
+      {/* <dinner></dinner> */}
+      <Dinner dishname='Chicken karahi'/> 
       
-        <h1>Bootcamp 2020 React app</h1>
-        <h2>
-          Hello Devrlopers.
-        </h2>
-        <h1>Bootcamp 2020 React app</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+       
   );
 }
 
---
+++ src/Dinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dinner (props){
+     return (
+        <div>
+             <h1>Today we are serving{props.dishname}</h1>
+             
+        </div>
+     );
+    
+}
+
+export default App;
